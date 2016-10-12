554fb93784f9f63143b2fa8482d5b21166f729b4
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-12 18:19:33 -0300

    Added project.project

--- models.py
+++ models.py
@@ -14,7 +14,12 @@ _logger = logging.getLogger(__name__)
 class project_materials(models.Model):
 	_name = 'project.materials'
 
+	project_id = fields.Many2one('project.project')
 	product_id = fields.Many2one('product.product')
 	qty_budget = fields.Float(string='Cantidad Presupuestada')
 	qty_consumed = fields.Float(string='Cantidad Consumida',readonly=True)
 
+class project_project(models.Model):
+	_inherit = 'project.project'
+
+	material_ids = fields.One2many(comodel_name='project.materials',inverse_name='project_id')
