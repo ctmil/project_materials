50b0e51ea85dc6d9ce905e074c6c2bd176e6ab4e
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-12 19:08:11 -0300

    Added dependency

--- __openerp__.py
+++ __openerp__.py
@@ -25,7 +25,7 @@
     'version': '9.0.1.0.0',
     'category': 'project',
     'license': 'AGPL-3',
-    'depends': ['project'],
+    'depends': ['project','purchase'],
     'data': [
 	'project_view.xml'
     ],
