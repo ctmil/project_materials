7d5715f5e6c32bbe72b66b2fdbdf82a5cc055b8a
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-12 19:18:54 -0300

    Added computed method

--- models.py
+++ models.py
@@ -14,10 +14,19 @@ _logger = logging.getLogger(__name__)
 class project_materials(models.Model):
 	_name = 'project.materials'
 
+	@api.one
+	def _compute_qty_consumed(self):
+		return_value = 0
+		purchase_lines = self.env['purchase.order.line'].search([('account_analytic_id','=',self.id)])
+		for line in purchase_lines:
+			if line.order_id.state in ['purchase','done']:
+				return_value = return_value + line.product_qty
+		self.qty_consumed = return_value
+
 	project_id = fields.Many2one('project.project')
 	product_id = fields.Many2one('product.product',string='Producto')
 	qty_budget = fields.Float(string='Cantidad Presupuestada')
-	qty_consumed = fields.Float(string='Cantidad Consumida')
+	qty_consumed = fields.Float(string='Cantidad Consumida',compute=_compute_qty_consumed)
 
 class project_project(models.Model):
 	_inherit = 'project.project'
