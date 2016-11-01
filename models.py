dca4303320b058b2a52ff5259e426a6f1eb6eb29
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-01 20:09:19 -0300

    Made consumed materials recursive

--- models.py
+++ models.py
@@ -17,12 +17,8 @@ class project_materials(models.Model):
 	@api.one
 	def _compute_qty_consumed(self):
 		return_value = 0
-		for project in self.project_id.child_ids:
-			purchase_lines = self.env['purchase.order.line'].search([('account_analytic_id','=',project.analytic_account_id.id),\
-					('product_id','=',self.product_id.id)])
-			for line in purchase_lines:
-				if line.order_id.state in ['purchase','done']:
-					return_value = return_value + line.product_qty
+		if self.project_id:
+			return_value = self.project_id._consumed_materials(0)
 		self.qty_consumed = return_value
 
 	@api.one
@@ -47,3 +43,16 @@ class project_project(models.Model):
 
 	material_ids = fields.One2many(comodel_name='project.materials',inverse_name='project_id',readonly=True,\
 			states={'draft': [('readonly', False)],'open': [('readonly', False)]})
+
+	@api.one
+	def _consumed_materials(self,qty = 0):
+		if self.child_ids:
+			for project in self.child_ids:
+				qty = qty + project._consumed_materials(qty)
+		else:
+			purchase_lines = self.env['purchase.order.line'].search([('account_analytic_id','=',self.analytic_account_id.id),\
+					('product_id','=',self.product_id.id)])
+			for line in purchase_lines:
+				if line.order_id.state in ['purchase','done']:
+					qty = qty + line.product_qty
+			return qty
